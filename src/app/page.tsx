44710d3215a7b3ffe4d6f3c193a5182796829977
MHashirAbdullah <hashirabdullah46@gmail.com>
2025-10-31 00:00:02 +0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen min-w-screen flex justify-center items-center'>
+      <Button>
+        click me
+      </Button>
+    </div>
+  )
+}
+
+export default page
