34cbdb0064734c6fa6847fede974fc171279a0ba
MHashirAbdullah <hashirabdullah46@gmail.com>
2025-11-02 01:50:11 +0500

    Setup trpc, created a procedure with prisma api, explore trpc client-side and server-side their and prefetch.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import { Button } from '@/components/ui/button'
-import prisma from '@/lib/db'
-import React from 'react'
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 const page = async () => {
-  const users = await prisma.user.findMany();
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
   return (
-    <div className='min-h-screen min-w-screen flex justify-center items-center'>
-        {JSON.stringify(users)}
+    <div className="min-h-screen min-w-screen flex justify-center items-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>loading...</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
