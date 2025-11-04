74aeb1ff075f2b5846a7897370d16654ebff85fd
MHashirAbdullah <hashirabdullah46@gmail.com>
2025-11-05 00:15:09 +0500

    setup inngest, create a background job, add mprocs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 const Page = () => {
-  const { data } = authClient.useSession();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const createWorkflow = useMutation(trpc.createWorkflow.mutationOptions(
+    {
+      onSuccess: ()=>{
+        toast.success("Workflow creation triggered");
+      }
+    }
+  ));
   return (
-    <div className="min-h-screen min-w-screen flex justify-center items-center">
-      {JSON.stringify(data)}
-      {data && (
-        <Button onClick={() => authClient.signOut()}>LogOut</Button>
-      )}
+    <div className="min-h-screen min-w-screen flex justify-center items-center flex-col gap-y-6">
+      <div>{JSON.stringify(data)}</div>
+      <Button
+        onClick={() => createWorkflow.mutate()}
+        disabled={createWorkflow.isPending}
+      >
+        Create workflow
+      </Button>
+      {data && <Button onClick={() => authClient.signOut()}>LogOut</Button>}
     </div>
   );
 };
