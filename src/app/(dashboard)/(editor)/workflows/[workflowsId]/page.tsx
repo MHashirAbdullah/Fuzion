b2b40a76402b7c6fd6aee05c7044d010e6dcd790
MHashirAbdullah <hashirabdullah46@gmail.com>
2025-11-23 03:17:41 +0500

    add feat: introduced a full-page workflow editor with header, breadcrumbs, and inline name editing. added saved action in editor header with clear disbled states during saving. provided loading and new error views for a smooth editing experience. Bug Fix: improved handling for missing workflows: user now see a clear error when a workflow doesnot exist. Summary: Implement workflow editor with error handling and name update functionality.

--- src/app/(dashboard)/(editor)/workflows/[workflowsId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowsId]/page.tsx
@@ -1,15 +1,35 @@
+import { prefetchWorkflow } from "@/features/workflows/server/prefectch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import {
+  Editor,
+  EditorError,
+  EditorLoading,
+} from "@/features/editor/components/editor";
+import { EditorHeader } from "@/features/editor/components/editor-header";
 
 interface PageProps {
   params: Promise<{
     workflowsId: string;
   }>;
-};
+}
 const Page = async ({ params }: PageProps) => {
   await requireAuth();
   const { workflowsId } = await params;
+  prefetchWorkflow(workflowsId);
   return (
-  <p>Workflows ID: {workflowsId}</p>
-  )
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorError />}>
+        <Suspense fallback={<EditorLoading />}>
+          <EditorHeader workflowId={workflowsId}/>
+          <main className="flex-1">
+            <Editor workflowId={workflowsId} />
+          </main>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 export default Page;
