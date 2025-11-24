d2cc450b96aea6b5c71dd8910f23a6ac0155fabb
MHashirAbdullah <hashirabdullah46@gmail.com>
2025-11-24 14:43:50 +0500

    new feat: Introduces an interactive workflow editor with node-based canvas, including pan/zoom controls, background grid and mini map. New workflows now start with initial node displayed in canvas. Nodes feature a toolbar with settings and delete actions; optional name and description shown in bottom. Add a new node button in the panel. Visual refinements for node structure and placeholders to support future node types. Summary: Add initial node components and database schema for workflow management.

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -6,13 +6,23 @@ import {
 } from "@/trpc/init";
 import { generateSlug } from "random-word-slugs";
 import z from "zod";
+import type { Node, Edge } from "@xyflow/react";
 import { PAGINATION } from "@/config/constants";
+import { NodeType } from "@/generated/prisma/enums";
+// import { Node } from "@/generated/prisma/client";
 export const workflowsRouter = createTRPCRouter({
   create: premiumProcedure.mutation(({ ctx }) => {
     return prisma.workflow.create({
       data: {
         name: generateSlug(3) || "New Workflow",
         userId: ctx.auth.user.id,
+        node: {
+          create: {
+            type: NodeType.INITIAL,
+            position: { x: 0, y: 0 },
+            name: NodeType.INITIAL,
+          },
+        },
       },
     });
   }),
@@ -44,13 +54,36 @@ export const workflowsRouter = createTRPCRouter({
 
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
-    .query(({ ctx, input }) => {
-      return prisma.workflow.findUniqueOrThrow({
+    .query(async ({ ctx, input }) => {
+      const workflow = await prisma.workflow.findUniqueOrThrow({
         where: {
           id: input.id,
           userId: ctx.auth.user.id,
         },
+        include: { node: true, connections: true },
       });
+      //transforming server nodes to react-flow comaptible nodes
+      const node: Node[] = workflow.node.map((node) =>({
+        id: node.id,
+        type: node.type,
+        position: node.position as { x: number; y: number },
+        data: (node.data as Record<string, unknown>) || {},
+      }));
+
+      //transforming server connection to react-flow comaptible edges
+      const edges: Edge[] = workflow.connections.map((connection) => ({
+        id: connection.id,
+        source: connection.formNodeId,
+        target: connection.toNodeId,
+        sourceHandle: connection.fromOutput,
+        targetHandle: connection.toInput,
+      }))
+      return {
+        id: workflow.id,
+        name: workflow.name,
+        node,
+        edges,
+      }
     }),
 
   getMany: protectedProcedure
