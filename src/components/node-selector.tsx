8a92b8d02e33877f674d5a64a64739004ccce953
MHashirAbdullah <hashirabdullah46@gmail.com>
2025-11-26 02:22:17 +0500

    Add feat: Added a node selector panel to insert workflow nodes from the canvas and add-button, Introduced new nodes type: Manual and Http, Added base components for trigger and execution nodes with consistent styling and connection handles, Enhance initial node to open the  Node selector for quick starting points, Mapped new node types to their visual components in editor, Added lightweight ID generation library. Updated db schema to support the new node types. Summary: Add new node types and integrate cuid2 package in the project.

---
+++ src/components/node-selector.tsx
@@ -0,0 +1,202 @@
+"use client";
+import { createId } from "@paralleldrive/cuid2";
+import { useReactFlow } from "@xyflow/react";
+import { GlobeIcon, MousePointerIcon } from "lucide-react";
+import { useCallback } from "react";
+import { toast } from "sonner";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { NodeType } from "@/generated/prisma/enums";
+import { Separator } from "./ui/separator";
+
+export type NodeTypeOptions = {
+  type: NodeType;
+  label: string;
+  description: string;
+  icon: React.ComponentType<{ className?: string }> | string;
+};
+
+const triggerNodes: NodeTypeOptions[] = [
+  {
+    type: NodeType.MANUAL_TRIGGER,
+    label: "Manual Trigger",
+    description: "Start the workflow manually",
+    icon: MousePointerIcon,
+  },
+];
+
+const executionNodes: NodeTypeOptions[] = [
+  {
+    type: NodeType.HTTP_REQUEST,
+    label: "HTTP Request",
+    description: "Make an HTTP request to an API endpoint",
+    icon: GlobeIcon,
+  },
+];
+
+interface NodeSelectorProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  children: React.ReactNode;
+}
+
+export function NodeSelector({
+  open,
+  onOpenChange,
+  children,
+}: NodeSelectorProps) {
+  const { setNodes, getNodes, screenToFlowPosition } = useReactFlow();
+  const handleNodeSelect = useCallback(
+    (selection: NodeTypeOptions) => {
+      if (selection.type === NodeType.MANUAL_TRIGGER) {
+        const nodes = getNodes();
+        const hasManualTrigger = nodes.some(
+          (node) => node.type === NodeType.MANUAL_TRIGGER
+        );
+        if (hasManualTrigger) {
+          toast.error(
+            "A Manual Trigger already exists in the workflow. Only one is allowed."
+          );
+          return;
+        }
+      }
+      setNodes((nodes) => {
+        const hasInitialTrigger = nodes.some(
+          (node) => node.type === NodeType.INITIAL
+        );
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 2;
+        const flowPosition = screenToFlowPosition({
+          x: centerX + (Math.random() - 0.5) * 200,
+          y: centerY + (Math.random() - 0.5) * 200,
+        });
+
+        const newNode = {
+          id: createId(),
+          data: {},
+          position: flowPosition,
+          type: selection.type,
+        };
+
+        if (hasInitialTrigger) {
+          return [newNode];
+        }
+        return [...nodes, newNode];
+      });
+      onOpenChange(false);
+    },
+    [setNodes, getNodes, onOpenChange, screenToFlowPosition]
+  );
+
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent
+        side="right"
+        className="w-full sm:max-w-md  overflow-y-auto"
+      >
+        <SheetHeader>
+          <SheetTitle>What triggers this workflow?</SheetTitle>
+          <SheetDescription>
+            A Trigger is a step that starts your workflow.
+          </SheetDescription>
+        </SheetHeader>
+        <div>
+          {triggerNodes.map((nodeType) => {
+            const Icon = nodeType.icon;
+            return (
+              <div
+                key={nodeType.type}
+                className="
+                          group
+                          w-full justify-start h-auto py-5 px-4 cursor-pointer
+                          border-l-2 border-transparent
+                          transition-all duration-300 ease-out
+                          hover:border-primary
+                          hover:bg-accent/50
+                          hover:shadow-md
+                          hover:-translate-y-[2px]
+                          active:scale-[0.98]
+                          rounded-md
+                        "
+                onClick={() => handleNodeSelect(nodeType)}
+              >
+                <div className="flex items-center gap-6 w-full overflow-hidden">
+                  {typeof Icon === "string" ? (
+                    <img
+                      src={Icon}
+                      alt={nodeType.label}
+                      className="size-5 object-contain rounded-sm transition-transform duration-300 group-hover:scale-110"
+                    />
+                  ) : (
+                    <Icon className="size-5 transition-transform duration-300 group-hover:scale-110" />
+                  )}
+
+                  <div className="flex flex-col text-left">
+                    <span className="font-medium text-sm">
+                      {nodeType.label}
+                    </span>
+                    <span className="text-xs text-muted-foreground">
+                      {nodeType.description}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <Separator />
+
+        <div>
+          {executionNodes.map((nodeType) => {
+            const Icon = nodeType.icon;
+            return (
+              <div
+                key={nodeType.type}
+                className="
+                group
+                w-full justify-start h-auto py-5 px-4 cursor-pointer
+                border-l-2 border-transparent
+                transition-all duration-300 ease-out
+                hover:border-primary
+                hover:bg-accent/50
+                hover:shadow-md
+                hover:-translate-y-[2px]
+                active:scale-[0.98]
+                rounded-md
+              "
+                onClick={() => handleNodeSelect(nodeType)}
+              >
+                <div className="flex items-center gap-6 w-full overflow-hidden">
+                  {typeof Icon === "string" ? (
+                    <img
+                      src={Icon}
+                      alt={nodeType.label}
+                      className="size-5 object-contain rounded-sm transition-transform duration-300 group-hover:scale-110"
+                    />
+                  ) : (
+                    <Icon className="size-5" />
+                  )}
+                  <div className="flex flex-col items-center text-left">
+                    <span className="font-medium text-sm">
+                      {nodeType.label}
+                    </span>
+                    <span className="text-xs text-muted-foreground">
+                      {nodeType.description}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
